30067eb45162b1527f1a87917c2aebd84f46272d
Felipe Ramos <feliperamos4@gmail.com>
2019-05-25 20:27:51 -0700

    adding files

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
+
+const App = () => <Title><h1>Front end App</h1></Title>;
+
+export default App;
